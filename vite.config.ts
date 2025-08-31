548f18dfde0b14a4859d53d82bc2bad9e0e10ab7
Priyanshu Tiwari <Priyanshutiwari878@yahoo.com>
2025-08-31 23:30:59 +0530

    #build: {     outDir: 'build', // <-- make Vite build to "build" instead of "dist"     chunkSizeWarningLimit: 2000, // optional: adjust warning   },

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'build', // <-- make Vite build to "build" instead of "dist"
+    chunkSizeWarningLimit: 2000, // optional: adjust warning
+  },
 });
