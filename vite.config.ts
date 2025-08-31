f6a7cfe6bfded09746a890576e5171f11ef8de49
Priyanshu Tiwari <Priyanshutiwari878@yahoo.com>
2025-08-31 23:33:37 +0530

    removed vercel json

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'build', // <-- make Vite build to "build" instead of "dist"
-    chunkSizeWarningLimit: 2000, // optional: adjust warning
-  },
+  // build: {
+  //   outDir: 'build',
+  //   chunkSizeWarningLimit: 2000,
+  // },
 });
